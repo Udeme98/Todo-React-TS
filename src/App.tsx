5e1354a9239927511f358029bbf81ad0d8a2116e
Udeme98 <Darealudeme1@gmail.com>
2024-07-30 21:25:46 +0100

    styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import InputField from "./component/InputField";
+
 const App = () => {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField />
     </div>
   );
 };
