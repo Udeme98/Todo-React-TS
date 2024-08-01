1bfb931122f9fa26ddeae4f8baadc06b2fa9e904
Udeme98 <Darealudeme1@gmail.com>
2024-08-01 07:12:31 +0100

    state to manage todos

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,13 @@ const App = () => {
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
+      setTodo("");
+    }
+
+    console.log(todos);
   };
 
   return (
--- src/component/InputField.tsx
+++ src/component/InputField.tsx
@@ -1,3 +1,5 @@
+import { useRef } from "react";
+
 type TodoProps = {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
@@ -5,10 +7,19 @@ type TodoProps = {
 };
 
 const InputField = ({ todo, setTodo, handleAdd }: TodoProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input" onSubmit={handleAdd}>
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <input
         type="input"
+        ref={inputRef}
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
         placeholder="Enter a task"
