818201e56ce89433a9628f6a59b5505491b60145
Udeme98 <Darealudeme1@gmail.com>
2024-08-03 06:52:21 +0100

    using useReducer with Typescript

---
+++ src/component/UseReducer.tsx
@@ -0,0 +1,31 @@
+import { useReducer } from "react";
+import { TodoModel } from "../todoModel";
+
+type Actions =
+  | { type: "add"; payload: string }
+  | { type: "remove"; payload: number }
+  | { type: "done"; payload: number };
+
+const initialState: TodoModel[] = [];
+
+const TodoReducer = (state: TodoModel[], action: Actions) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        { id: Date.now(), todo: action.payload, isDone: false },
+      ];
+    case "remove":
+      return state.filter((todo) => todo.id !== action.payload);
+    case "done":
+      return state.map((todo) =>
+        todo.id !== action.payload ? { ...todo, isDone: !todo.isDone } : todo
+      );
+  }
+};
+
+const UseReducer = () => {
+  const [state, disptch] = useReducer(TodoReducer, initialState);
+  return <div></div>;
+};
+export default UseReducer;
